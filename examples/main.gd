0df638823d0a8abb6fe0598a4322cc2640eadbf5
Alek Mlynek <alek@swiftroot.com>
2019-02-21 13:58:35 -0700

    Cleaned up example project.

--- examples/main.gd
+++ examples/main.gd
@@ -2,13 +2,10 @@ extends Control
 var hmac = HMAC.new()
 
 func _ready():
-	var hello = "String"
-	hello.to_utf8()
+	pass
 	
 func _cb_encrypt():
-	hmac.init($panel/secret.text.to_utf8(), $panel/payload.text.to_utf8(), 6)
+	hmac.init($panel/secret.text.to_utf8(), $panel/payload.text.to_utf8(), $panel/hash_type.text)
 	hmac.digest()
-	var hmacvalue = hmac.get_hmac()
-	print("Local print")
-	print(hmacvalue)
-
+	print(str("Your HMAC: ", hmac.get_hmac()))
+	$panel/result.text = hmac.get_hmac()
